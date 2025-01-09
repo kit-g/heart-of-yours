c3342e36a7f03eb325e8afdb28f49f69559bbbf0
_kitg <gerasimovkit@gmail.com>
2025-01-09 13:54:00 -0500

    chore: sentry is switched off in debug mode

--- lib/core/env/config.dart
+++ lib/core/env/config.dart
@@ -19,4 +19,6 @@ class AppConfig {
   static Env env = Env.fromString(const String.fromEnvironment('ENV').trim());
   static const String logLevel = String.fromEnvironment('LOG_LEVEL');
   static const String sentryDsn = String.fromEnvironment('SENTRY_DSN');
+
+  static bool get isProd => env == Env.prod;
 }
--- lib/core/env/sentry.dart
+++ lib/core/env/sentry.dart
@@ -5,16 +5,17 @@ import 'package:heart/core/env/config.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> initSentry(FutureOr<void> Function() appRunner) {
+  if (kDebugMode) return Future.value(null);
   return SentryFlutter.init(
     (options) {
       options
-        ..debug = kDebugMode
+        ..debug = !AppConfig.isProd
         ..enableAutoPerformanceTracing = true
         ..enableWatchdogTerminationTracking = true
         ..enableMemoryPressureBreadcrumbs = true
         ..dsn = AppConfig.sentryDsn
-        ..tracesSampleRate = kDebugMode ? null : 1.0
-        ..profilesSampleRate = kDebugMode ? null : 1.0
+        ..tracesSampleRate = 1.0
+        ..profilesSampleRate = 1.0
         ..diagnosticLevel = switch (AppConfig.env) {
           Env.dev => SentryLevel.debug,
           Env.prod => SentryLevel.error,
