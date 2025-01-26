ebb5583cc6298c17ba739eaa6b6a4381274999a1
_kitg <gerasimovkit@gmail.com>
2025-01-26 15:53:19 -0500

    feat: new icon font chore: login.dart modifications

--- lib/presentation/routes/login.dart
+++ lib/presentation/routes/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:heart/core/env/sentry.dart';
+import 'package:heart/core/utils/icons.dart';
 import 'package:heart/core/utils/visual.dart';
 import 'package:heart_language/heart_language.dart';
 import 'package:heart_state/heart_state.dart';
@@ -25,6 +26,8 @@ class _LoginPageState extends State<LoginPage> with LoadingState<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final L(:logInWithGoogle, :logInWithApple) = L.of(context);
+    final ThemeData(textTheme: TextTheme(titleMedium: style)) = Theme.of(context);
+
     return Scaffold(
       body: Center(
         child: ValueListenableBuilder<bool>(
@@ -40,30 +43,69 @@ class _LoginPageState extends State<LoginPage> with LoadingState<LoginPage> {
               },
             );
           },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 24,
-            children: [
-              OutlinedButton(
-                onPressed: () => _logIn(context, Auth.of(context).loginWithGoogle),
-                child: Text(logInWithGoogle),
-              ),
-              FutureBuilder<bool>(
-                future: _isAppleSignNnAvailable,
-                builder: (_, snapshot) {
-                  return AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 100),
-                    child: switch (!snapshot.hasError && snapshot.hasData && snapshot.data!) {
-                      false => const SizedBox.shrink(),
-                      true => OutlinedButton(
-                          onPressed: () => _logIn(context, Auth.of(context).loginWithApple),
-                          child: Text(logInWithApple),
-                        ),
-                    },
-                  );
-                },
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 60.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 24,
+              children: [
+                Stack(
+                  children: [
+                    const Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 24,
+                      child: Icon(CustomIcons.google),
+                    ),
+                    OutlinedButton(
+                      onPressed: () => _logIn(context, Auth.of(context).loginWithGoogle),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            logInWithGoogle,
+                            style: style,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                FutureBuilder<bool>(
+                  future: _isAppleSignNnAvailable,
+                  builder: (_, snapshot) {
+                    return AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 100),
+                      child: switch (!snapshot.hasError && snapshot.hasData && snapshot.data!) {
+                        false => const SizedBox.shrink(),
+                        true => Stack(
+                            children: [
+                              const Positioned(
+                                top: 0,
+                                bottom: 0,
+                                left: 24,
+                                child: Icon(CustomIcons.appstore),
+                              ),
+                              OutlinedButton(
+                                onPressed: () => _logIn(context, Auth.of(context).loginWithApple),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      logInWithApple,
+                                      style: style,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
