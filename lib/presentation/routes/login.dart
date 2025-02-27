5c109397463a0d3c5605b040db0ded081a089044
_kitg <gerasimovkit@gmail.com>
2025-02-26 19:34:07 -0500

    chore: layout made scrollable

--- lib/presentation/routes/login.dart
+++ lib/presentation/routes/login.dart
@@ -43,90 +43,67 @@ class _LoginPageState extends State<LoginPage> with LoadingState<LoginPage> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const Expanded(
-              flex: 2,
-              child: Logo(),
-            ),
-            Expanded(
-              flex: 5,
-              child: ValueListenableBuilder<bool>(
-                valueListenable: loader,
-                builder: (_, loading, child) {
-                  return AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 200),
-                    child: switch (loading) {
-                      false => child!,
-                      true => const Center(
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        )
-                    },
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 60.0),
+        child: LayoutBuilder(
+          builder: (_, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      _Form(
-                        formKey: _formKey,
-                        emailController: _emailController,
-                        passwordController: _passwordController,
-                        onLogin: _logInWithEmail,
-                        obscurityController: _passwordObscurityController,
-                        error: _loginError,
+                      const Expanded(
+                        flex: 2,
+                        child: Logo(),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(orConnector),
-                      ),
-                      Stack(
-                        children: [
-                          const Positioned(
-                            top: 0,
-                            bottom: 0,
-                            left: 24,
-                            child: Icon(CustomIcons.google),
-                          ),
-                          OutlinedButton(
-                            onPressed: () => _logIn(Auth.of(context).loginWithGoogle),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                      Expanded(
+                        flex: 5,
+                        child: ValueListenableBuilder<bool>(
+                          valueListenable: loader,
+                          builder: (_, loading, child) {
+                            return AnimatedSwitcher(
+                              duration: const Duration(milliseconds: 200),
+                              child: switch (loading) {
+                                false => child!,
+                                true => const Center(
+                                    child: CircularProgressIndicator(strokeWidth: 2),
+                                  )
+                              },
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text(
-                                  logInWithGoogle,
-                                  style: style,
+                                _Form(
+                                  formKey: _formKey,
+                                  emailController: _emailController,
+                                  passwordController: _passwordController,
+                                  onLogin: _logInWithEmail,
+                                  obscurityController: _passwordObscurityController,
+                                  error: _loginError,
                                 ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      FutureBuilder<bool>(
-                        future: _isAppleSignNnAvailable,
-                        builder: (_, snapshot) {
-                          final AsyncSnapshot(:hasData, :hasError, data: available) = snapshot;
-                          bool hasAppleSignIn = _isIos(context) && !hasError && hasData && available!;
-                          return AnimatedSwitcher(
-                            duration: const Duration(milliseconds: 100),
-                            child: switch (hasAppleSignIn) {
-                              false => const SizedBox.shrink(),
-                              true => Stack(
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(orConnector),
+                                ),
+                                Stack(
                                   children: [
                                     const Positioned(
                                       top: 0,
                                       bottom: 0,
                                       left: 24,
-                                      child: Icon(CustomIcons.appstore),
+                                      child: Icon(CustomIcons.google),
                                     ),
                                     OutlinedButton(
-                                      onPressed: () => _logIn(Auth.of(context).loginWithApple),
+                                      onPressed: () => _logIn(Auth.of(context).loginWithGoogle),
                                       child: Row(
                                         mainAxisAlignment: MainAxisAlignment.center,
                                         children: [
                                           Text(
-                                            logInWithApple,
+                                            logInWithGoogle,
                                             style: style,
                                           ),
                                         ],
@@ -134,16 +111,52 @@ class _LoginPageState extends State<LoginPage> with LoadingState<LoginPage> {
                                     ),
                                   ],
                                 ),
-                            },
-                          );
-                        },
+                                FutureBuilder<bool>(
+                                  future: _isAppleSignNnAvailable,
+                                  builder: (_, snapshot) {
+                                    final AsyncSnapshot(:hasData, :hasError, data: available) = snapshot;
+                                    bool hasAppleSignIn = _isIos(context) && !hasError && hasData && available!;
+                                    return AnimatedSwitcher(
+                                      duration: const Duration(milliseconds: 100),
+                                      child: switch (hasAppleSignIn) {
+                                        false => const SizedBox.shrink(),
+                                        true => Stack(
+                                            children: [
+                                              const Positioned(
+                                                top: 0,
+                                                bottom: 0,
+                                                left: 24,
+                                                child: Icon(CustomIcons.appstore),
+                                              ),
+                                              OutlinedButton(
+                                                onPressed: () => _logIn(Auth.of(context).loginWithApple),
+                                                child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: [
+                                                    Text(
+                                                      logInWithApple,
+                                                      style: style,
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
