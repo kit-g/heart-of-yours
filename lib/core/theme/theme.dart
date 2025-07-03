08ca3250d0ba4759c1b0af8ed433577ff9ceabf5
_kitg <gerasimovkit@gmail.com>
2025-07-02 21:33:26 -0400

    chore: minor polish of theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -19,35 +20,23 @@ ThemeData theme(ColorScheme colorScheme) {
     canvasColor: colorScheme.surface,
     inputDecorationTheme: InputDecorationTheme(
       isDense: true,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(6),
-        borderSide: BorderSide.none,
-      ),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(6), borderSide: BorderSide.none),
       filled: true,
       suffixStyle: textTheme.labelSmall,
     ),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       elevation: 1,
-      selectedIconTheme: IconThemeData(
-        color: colorScheme.onSurface,
-        size: 28,
-      ),
+      selectedIconTheme: IconThemeData(color: colorScheme.onSurface, size: 28),
       selectedLabelStyle: textTheme.bodyLarge,
       selectedItemColor: colorScheme.onSurface,
       showUnselectedLabels: true,
-      unselectedIconTheme: IconThemeData(
-        color: colorScheme.outline,
-        size: 24,
-      ),
+      unselectedIconTheme: IconThemeData(color: colorScheme.outline, size: 24),
       unselectedLabelStyle: textTheme.bodySmall,
       unselectedItemColor: colorScheme.outline,
     ),
     bottomSheetTheme: BottomSheetThemeData(
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: _widerRadius,
-          topRight: _widerRadius,
-        ),
+        borderRadius: BorderRadius.only(topLeft: _widerRadius, topRight: _widerRadius),
       ),
       backgroundColor: colorScheme.surfaceContainerLow,
     ),
@@ -75,9 +64,7 @@ ThemeData theme(ColorScheme colorScheme) {
       ),
       brightness: colorScheme.brightness,
     ),
-    toggleButtonsTheme: const ToggleButtonsThemeData(
-      borderRadius: BorderRadius.all(Radius.circular(4)),
-    ),
+    toggleButtonsTheme: const ToggleButtonsThemeData(borderRadius: BorderRadius.all(Radius.circular(4))),
     snackBarTheme: SnackBarThemeData(
       backgroundColor: colorScheme.inverseSurface,
       contentTextStyle: TextStyle(
@@ -87,9 +74,7 @@ ThemeData theme(ColorScheme colorScheme) {
         fontWeight: FontWeight.w400,
       ),
       behavior: SnackBarBehavior.floating,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(4)),
-      ),
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(4))),
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
@@ -97,9 +82,7 @@ ThemeData theme(ColorScheme colorScheme) {
         minimumSize: const Size(0, 0),
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
         visualDensity: VisualDensity.compact,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
         textStyle: textTheme.bodyMedium?.copyWith(fontFamily: _font),
       ),
     ),
@@ -111,23 +94,29 @@ ThemeData theme(ColorScheme colorScheme) {
       ),
     ),
     progressIndicatorTheme: const ProgressIndicatorThemeData(strokeWidth: 2),
+    navigationRailTheme: const NavigationRailThemeData(
+      indicatorShape: CircleBorder(),
+    ),
   );
 }
 
 TextTheme _textTheme({Color? primaryColor, Color? secondaryColor}) {
+  final regular = Platform.isMacOS ? FontWeight.w200 : FontWeight.w400;
+  final bold = Platform.isMacOS ? FontWeight.w500 : FontWeight.w700;
+
   return TextTheme(
     displayLarge: TextStyle(fontSize: 96.0, fontWeight: FontWeight.w300, letterSpacing: -1.5, color: primaryColor),
     displayMedium: TextStyle(fontSize: 60.0, fontWeight: FontWeight.w300, letterSpacing: -0.5, color: primaryColor),
-    displaySmall: TextStyle(fontSize: 48.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, color: primaryColor),
-    headlineMedium: TextStyle(fontSize: 34.0, fontWeight: FontWeight.w700, letterSpacing: 0.25, color: primaryColor),
-    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, color: primaryColor),
-    titleLarge: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700, letterSpacing: 0.15, color: primaryColor),
-    titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700, letterSpacing: 0.15, color: primaryColor),
-    titleSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700, letterSpacing: 0.1, color: primaryColor),
-    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, letterSpacing: 0.5, color: primaryColor),
-    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, letterSpacing: 0.25, color: primaryColor),
-    bodySmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, letterSpacing: 0.4, color: secondaryColor),
-    labelLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700, letterSpacing: 0.25, color: secondaryColor),
-    labelSmall: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w400, letterSpacing: 1.5, color: secondaryColor),
+    displaySmall: TextStyle(fontSize: 48.0, fontWeight: regular, letterSpacing: 0.0, color: primaryColor),
+    headlineMedium: TextStyle(fontSize: 34.0, fontWeight: bold, letterSpacing: 0.25, color: primaryColor),
+    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: regular, letterSpacing: 0.0, color: primaryColor),
+    titleLarge: TextStyle(fontSize: 20.0, fontWeight: bold, letterSpacing: 0.15, color: primaryColor),
+    titleMedium: TextStyle(fontSize: 16.0, fontWeight: bold, letterSpacing: 0.15, color: primaryColor),
+    titleSmall: TextStyle(fontSize: 14.0, fontWeight: bold, letterSpacing: 0.1, color: primaryColor),
+    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: regular, letterSpacing: 0.5, color: primaryColor),
+    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: regular, letterSpacing: 0.25, color: primaryColor),
+    bodySmall: TextStyle(fontSize: 12.0, fontWeight: regular, letterSpacing: 0.4, color: secondaryColor),
+    labelLarge: TextStyle(fontSize: 14.0, fontWeight: bold, letterSpacing: 0.25, color: secondaryColor),
+    labelSmall: TextStyle(fontSize: 10.0, fontWeight: regular, letterSpacing: 1.5, color: secondaryColor),
   );
 }
