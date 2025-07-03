146c460c783df9afd85bef67a3b7aea164cee46a
_kitg <gerasimovkit@gmail.com>
2025-07-02 23:37:51 -0400

    feat: master detail view for history page

--- lib/presentation/navigation/router/router.dart
+++ lib/presentation/navigation/router/router.dart
@@ -187,8 +187,8 @@ RouteBase _historyRoute() {
             path: ':workoutId',
             builder: (context, state) {
               try {
-                final workoutId = state.pathParameters['workoutId']!;
-                final workout = Workouts.of(context).lookup(workoutId);
+                final workoutId = state.pathParameters['workoutId'];
+                final workout = Workouts.of(context).lookup(workoutId!);
                 return WorkoutEditor(
                   copy: workout!.copy(sameId: true)..completeAllSets(),
                 );
@@ -197,13 +197,7 @@ RouteBase _historyRoute() {
               }
             },
             name: _historyEditName,
-            redirect: (context, state) {
-              return switch (state.pathParameters['workoutId']) {
-                String id when Workouts.of(context).lookup(id) != null => null,
-                _ => _historyPath,
-              };
-            },
-          ),
+          )
         ],
       ),
     ],
