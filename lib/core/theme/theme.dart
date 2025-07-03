de362e08f1d732fd49376ba5c712038e6d8bb155
_kitg <gerasimovkit@gmail.com>
2025-07-02 21:33:26 -0400

    chore: minor polish of theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
@@ -111,23 +113,29 @@ ThemeData theme(ColorScheme colorScheme) {
       ),
     ),
     progressIndicatorTheme: const ProgressIndicatorThemeData(strokeWidth: 2),
+    navigationRailTheme: const NavigationRailThemeData(
+      indicatorShape: CircleBorder(),
+    ),
   );
 }
 
 TextTheme _textTheme({Color? primaryColor, Color? secondaryColor}) {
+  final regular = Platform.isMacOS ? FontWeight.w200 : FontWeight.w400;
+  final bold = Platform.isMacOS ? FontWeight.w500 : FontWeight.w700;
+
   return TextTheme(
     displayLarge: TextStyle(fontSize: 96.0, fontWeight: FontWeight.w300, letterSpacing: -1.5, color: primaryColor),
     displayMedium: TextStyle(fontSize: 60.0, fontWeight: FontWeight.w300, letterSpacing: -0.5, color: primaryColor),
-    displaySmall: TextStyle(fontSize: 48.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, color: primaryColor),
-    headlineMedium: TextStyle(fontSize: 34.0, fontWeight: FontWeight.w700, letterSpacing: 0.25, color: primaryColor),
-    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400, letterSpacing: 0.0, color: primaryColor),
-    titleLarge: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700, letterSpacing: 0.15, color: primaryColor),
-    titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700, letterSpacing: 0.15, color: primaryColor),
-    titleSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700, letterSpacing: 0.1, color: primaryColor),
-    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, letterSpacing: 0.5, color: primaryColor),
-    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, letterSpacing: 0.25, color: primaryColor),
-    bodySmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, letterSpacing: 0.4, color: secondaryColor),
-    labelLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700, letterSpacing: 0.25, color: secondaryColor),
-    labelSmall: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w400, letterSpacing: 1.5, color: secondaryColor),
+    displaySmall: TextStyle(fontSize: 48.0, fontWeight: regular, letterSpacing: 0.0, color: primaryColor),
+    headlineMedium: TextStyle(fontSize: 34.0, fontWeight: bold, letterSpacing: 0.25, color: primaryColor),
+    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: regular, letterSpacing: 0.0, color: primaryColor),
+    titleLarge: TextStyle(fontSize: 20.0, fontWeight: bold, letterSpacing: 0.15, color: primaryColor),
+    titleMedium: TextStyle(fontSize: 16.0, fontWeight: bold, letterSpacing: 0.15, color: primaryColor),
+    titleSmall: TextStyle(fontSize: 14.0, fontWeight: bold, letterSpacing: 0.1, color: primaryColor),
+    bodyLarge: TextStyle(fontSize: 16.0, fontWeight: regular, letterSpacing: 0.5, color: primaryColor),
+    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: regular, letterSpacing: 0.25, color: primaryColor),
+    bodySmall: TextStyle(fontSize: 12.0, fontWeight: regular, letterSpacing: 0.4, color: secondaryColor),
+    labelLarge: TextStyle(fontSize: 14.0, fontWeight: bold, letterSpacing: 0.25, color: secondaryColor),
+    labelSmall: TextStyle(fontSize: 10.0, fontWeight: regular, letterSpacing: 1.5, color: secondaryColor),
   );
 }
