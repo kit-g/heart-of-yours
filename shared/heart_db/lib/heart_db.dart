dffc740b2e611eab44b3cff7183bcacb62d31aea
_kitg <gerasimovkit@gmail.com>
2025-07-06 19:51:51 -0400

    chore: sqflite works

--- shared/heart_db/lib/heart_db.dart
+++ shared/heart_db/lib/heart_db.dart
@@ -7,7 +7,7 @@ import 'package:heart_models/heart_models.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'src/sql.dart' as sql;
 
 export 'package:heart_db/heart_db.dart' show LocalDatabase;
