8b9d8802dc9c53d796e57da8f50960929b34ca59
_kitg <gerasimovkit@gmail.com>
2025-07-06 19:52:53 -0400

    chore: headers are web aware

--- lib/core/utils/headers.dart
+++ lib/core/utils/headers.dart
@@ -6,6 +6,7 @@ import 'package:heart/core/env/config.dart';
 Map<String, String> headers({
   String? sessionToken,
   String? appVersion,
+  bool isWeb = false,
 }) {
   return {
     if (sessionToken != null) 'Authorization': 'Bearer $sessionToken',
@@ -13,28 +14,32 @@ Map<String, String> headers({
     'Accept': 'application/json',
     'Accept-Language': PlatformDispatcher.instance.locale.toLanguageTag(),
     'X-Timezone': DateTime.now().timeZoneName,
-    ..._common(appVersion),
+    ..._common(appVersion, isWeb: isWeb),
   };
 }
 
-Map<String, String> imageHeaders({String? appVersion}) {
+Map<String, String> imageHeaders({String? appVersion, required bool isWeb}) {
   return {
     'Accept': 'image/avif,image/webp,image/png,image/jpeg,image/gif',
-    'Accept-Encoding': 'gzip, br, deflate',
+    if (!isWeb) 'Accept-Encoding': 'gzip, br, deflate',
     'Cache-Control': 'public, max-age=31536000, immutable',
-    ..._common(appVersion),
+    ..._common(appVersion, isWeb: isWeb),
   };
 }
 
-Map<String, String> _common(String? appVersion) {
+Map<String, String> _common(String? appVersion, {bool isWeb = false}) {
   final version = appVersion ?? 'Unknown version';
   return {
-    'Referer': AppConfig.appLink,
-    'User-Agent': _userAgent(version),
+    if (!isWeb) 'Referer': AppConfig.appLink,
+    if (!isWeb) 'User-Agent': _userAgent(version),
     if (appVersion != null) 'X-App-Version': appVersion,
   };
 }
 
 String _userAgent(String version) {
-  return '${AppConfig.appName}/$version (Flutter; ${Platform.operatingSystem}; +${AppConfig.appLink})';
+  try {
+    return '${AppConfig.appName}/$version (Flutter; ${Platform.operatingSystem}; +${AppConfig.appLink})';
+  } catch (e) {
+    return '${AppConfig.appName}/$version (Flutter; web; +${AppConfig.appLink})';
+  }
 }
