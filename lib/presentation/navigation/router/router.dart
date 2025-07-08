e6546c442d8a7e6e7317954125e81fafeb035f1a
_kitg <gerasimovkit@gmail.com>
2025-07-07 21:06:19 -0400

    feat: dynamic layout of the auth pages

--- lib/presentation/navigation/router/router.dart
+++ lib/presentation/navigation/router/router.dart
@@ -12,11 +12,14 @@ import 'package:heart/presentation/routes/profile/profile.dart';
 import 'package:heart/presentation/routes/settings/settings.dart';
 import 'package:heart/presentation/routes/workout/workout.dart';
 import 'package:heart/presentation/widgets/greetings_pane.dart';
+import 'package:heart/presentation/widgets/greetings_pane.dart';
 import 'package:heart/presentation/widgets/responsive/responsive_builder.dart';
 import 'package:heart/presentation/widgets/split_scaffold.dart';
 import 'package:heart_language/heart_language.dart';
 import 'package:heart/presentation/widgets/app_frame.dart';
 import 'package:heart/presentation/widgets/responsive/responsive_builder.dart';
+import 'package:heart/presentation/widgets/split_scaffold.dart';
+import 'package:heart_language/heart_language.dart';
 import 'package:heart_state/heart_state.dart';
 
 part 'animation.dart';
