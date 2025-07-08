80ebd453c48fbab6574e4144e7539ca041d0dbc6
_kitg <gerasimovkit@gmail.com>
2025-07-08 00:05:05 -0400

    chore: manual deployment script

---
+++ scripts/bucket.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# local build script, not reusable
+set -e
+
+BUCKET="583168578067-heart-app"
+DISTRIBUTION_ID="E2QX06VIJT572Y"
+
+flutter build web --release --dart-define-from-file="env/new-dev.json"
+aws s3 sync build/web "s3://$BUCKET" --delete --profile personal
+
+echo "Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --profile personal
+
+echo "Deployment complete!"
