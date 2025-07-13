347663db1fad0706dc5e88b07a59ea784947f1fc
_kitg <gerasimovkit@gmail.com>
2025-07-12 22:18:28 -0400

    fix: forgotten log out state

--- shared/heart_state/lib/src/clear.dart
+++ shared/heart_state/lib/src/clear.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'alarms.dart';
 import 'auth.dart';
+import 'config.dart';
 import 'exercises.dart';
+import 'previous.dart';
 import 'stats.dart';
 import 'templates.dart';
 import 'timers.dart';
@@ -12,6 +14,8 @@ void clearState(BuildContext context) {
   Alarms.of(context).onSignOut();
   Auth.of(context).onSignOut();
   Exercises.of(context).onSignOut();
+  PreviousExercises.of(context).onSignOut();
+  RemoteConfig.of(context).onSignOut();
   Stats.of(context).onSignOut();
   Templates.of(context).onSignOut();
   Timers.of(context).onSignOut();
