26a371610cc2d7420d2f0a377ac0658e74caa581
_kitg <gerasimovkit@gmail.com>
2025-07-23 02:16:10 -0400

    chore: db conflict

--- shared/heart_db/lib/src/db.dart
+++ shared/heart_db/lib/src/db.dart
@@ -15,15 +15,13 @@ class LocalDatabase
   static Future<LocalDatabase> init({int version = 1, Database? other, bool isWeb = false}) async {
     if (other != null) return LocalDatabase._(other);
 
-    var path = await getDatabasesPath();
-    // await deleteDatabase(path);
-
     const name = 'heart.db';
 
     final path = switch (isWeb) {
       true => 'heart',
       false => await getDatabasesPath(),
     };
+    // await deleteDatabase(path);
 
     if (isWeb) {
       databaseFactory = databaseFactoryFfiWeb;
