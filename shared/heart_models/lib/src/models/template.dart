9876583158d959e5ad1ae2321d7a68e8ca09060e
_kitg <gerasimovkit@gmail.com>
2025-07-25 14:11:53 -0400

    refactor: Template.fromRows removed
    (cherry picked from commit 9783e18527912ea96d43d9a4cf89594e57101b6e)

--- shared/heart_models/lib/src/models/template.dart
+++ shared/heart_models/lib/src/models/template.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:math';
 
 import 'exercise.dart';
@@ -22,37 +21,6 @@ abstract interface class Template
       exercises: [],
       id: id,
       order: order,
-    );
-  }
-
-  factory Template.fromRows(List<Map<String, dynamic>> rows) {
-    assert(rows.isNotEmpty, 'Cannot create a Template from an empty row list.');
-
-    final first = rows.first;
-
-    final exercises = rows.map<WorkoutExercise>(
-      (row) {
-        final exercise = Exercise.fromJson(row);
-        final desc = jsonDecode(row['description'] as String) as List;
-        final sets = desc.map<ExerciseSet>(
-          (json) {
-            return ExerciseSet.fromJson(exercise, json);
-          },
-        ).toList();
-        final workoutExercise = WorkoutExercise(starter: sets.firstOrNull ?? ExerciseSet(exercise));
-        for (var set in sets.skip(1)) {
-          workoutExercise.add(set);
-        }
-
-        return workoutExercise;
-      },
-    ).toList();
-
-    return _Template(
-      id: first['templateId'].toString(),
-      name: first['templateName'],
-      order: first['orderInParent'] as int? ?? 0,
-      exercises: exercises,
     );
   }
 
