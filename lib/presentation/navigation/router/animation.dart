daf6fa623f2496872246dc8d3c2b1eaaf74775c3
_kitg <gerasimovkit@gmail.com>
2025-08-09 20:53:01 -0400

    chore: unnecessary_underscores linter ignored

--- lib/presentation/navigation/router/animation.dart
+++ lib/presentation/navigation/router/animation.dart
@@ -1,6 +1,6 @@
 part of 'router.dart';
 
-Widget _pageTransition(__, Animation<double> animation, _, Widget child) {
+Widget _pageTransition(BuildContext __, Animation<double> animation, _, Widget child) {
   final scaleAnimation = Tween(begin: .8, end: 1.0).animate(
     CurvedAnimation(
       parent: animation,
