92057d036345ccbfb7399bcdf747e735578bb63e
_kitg <gerasimovkit@gmail.com>
2025-09-17 21:09:05 -0400

    feat: exercise archive list page

---
+++ lib/presentation/routes/exercises/archive.dart
@@ -0,0 +1,33 @@
+part of 'exercises.dart';
+
+class ExerciseArchive extends StatelessWidget {
+  final void Function(Exercise) onExercise;
+
+  const ExerciseArchive({
+    super.key,
+    required this.onExercise,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final L(:archivedExercises) = L.of(context);
+    final archived = Exercises.watch(context).archived.toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(archivedExercises),
+      ),
+      body: ListView.builder(
+        itemBuilder: (_, index) {
+          final each = archived[index];
+          return ExerciseItem(
+            exercise: each,
+            preferences: Preferences.watch(context),
+            selected: false,
+            onExerciseSelected: onExercise,
+          );
+        },
+        itemCount: archived.length,
+      ),
+    );
+  }
+}
