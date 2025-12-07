7c8b498a9a052b6e9b24168efb5062dbe41bbeee
_kitg <gerasimovkit@gmail.com>
2025-12-06 20:55:33 -0500

    feat: mobile upgrade icon and assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,8 @@
 abstract final class Assets {
+  // SVG
   static const String emptyWorkout = 'assets/svg/barbell.svg';
+  static const String mobileUpgrade = 'assets/svg/mobile_upgrade.svg';
+
+  // PNG
   static const String logo = 'assets/icons/launcher_main_macos.png';
 }
--- lib/presentation/routes/settings/upgrade_app.dart
+++ lib/presentation/routes/settings/upgrade_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:heart/core/utils/assets.dart';
+import 'package:heart/presentation/widgets/vector.dart';
 import 'package:heart_language/heart_language.dart';
 
 class UpgradeRequiredPage extends StatelessWidget {
@@ -7,10 +9,10 @@ class UpgradeRequiredPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final L(:updateRequiredBody, :updateRequiredTitle, :updateRequiredCta) = L.of(context);
-    final ThemeData(:textTheme, :platform) = Theme.of(context);
+    final ThemeData(:textTheme, :platform, :colorScheme) = Theme.of(context);
 
+    // keep untranslated
     final store = switch (platform) {
-      // keep untranslated
       .android => 'Play Store',
       .iOS || .macOS => 'App Store',
       _ => null,
@@ -23,7 +25,25 @@ class UpgradeRequiredPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(Icons.system_update, size: 64),
+              Container(
+                width: 96,
+                height: 96,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: colorScheme.primary,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: Vector(
+                    Assets.mobileUpgrade,
+                    height: 64,
+                    width: 64,
+                    color: colorScheme.primary,
+                  ),
+                ),
+              ),
               const SizedBox(height: 24),
               Text(
                 updateRequiredTitle,
