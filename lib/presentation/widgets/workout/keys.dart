58d4f079c483013c477b6bfeef77563a0cd75ac0
_kitg <gerasimovkit@gmail.com>
2025-12-10 23:22:46 -0500

    feat: "options" workout detail key

--- lib/presentation/widgets/workout/keys.dart
+++ lib/presentation/widgets/workout/keys.dart
@@ -4,6 +4,7 @@ class WorkoutDetailKeys {
   WorkoutDetailKeys._();
 
   static const cancelWorkout = Key('WorkoutDetail.cancelWorkout');
+  static const options = Key('WorkoutDetail.options');
   static const addExercises = Key('WorkoutDetail.addExercises');
   static const startNewWorkout = Key('WorkoutDetail.startNewWorkout');
   static const finishWorkout = Key('WorkoutDetail.finishWorkout');
