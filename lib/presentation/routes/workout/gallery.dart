e9b6278ed469e62b36efdd28e86850100af3db84
_kitg <gerasimovkit@gmail.com>
2025-12-12 19:57:13 -0500

    feat: gallery page in workout routes

---
+++ lib/presentation/routes/workout/gallery.dart
@@ -0,0 +1,41 @@
+part of 'workout.dart';
+
+class GalleryPage extends StatelessWidget {
+  final String? remote;
+  final Uint8List? bytes;
+
+  const GalleryPage({
+    super.key,
+    required this.remote,
+    required this.bytes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: Colors.white,
+          icon: const Icon(Icons.close_rounded),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: InteractiveViewer(
+          panEnabled: true,
+          scaleFactor: 2,
+          scaleEnabled: true,
+          child: AppImage(
+            url: remote,
+            bytes: bytes,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
