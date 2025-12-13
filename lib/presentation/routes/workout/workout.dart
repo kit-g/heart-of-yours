62f8f03951d06a733c6c2a0cdc2acfa371155976
_kitg <gerasimovkit@gmail.com>
2025-12-12 19:59:05 -0500

    feat: workout image tap callback

--- lib/presentation/routes/workout/workout.dart
+++ lib/presentation/routes/workout/workout.dart
@@ -6,6 +6,7 @@ import 'package:heart/core/utils/misc.dart';
 import 'package:heart/core/utils/visual.dart';
 import 'package:heart/presentation/widgets/appbar_textfield.dart';
 import 'package:heart/presentation/widgets/buttons.dart';
+import 'package:heart/presentation/widgets/image.dart';
 import 'package:heart/presentation/widgets/workout/timer.dart';
 import 'package:heart/presentation/widgets/workout/workout_detail.dart';
 import 'package:heart_language/heart_language.dart';
@@ -13,6 +14,7 @@ import 'package:heart_models/heart_models.dart';
 import 'package:heart_state/heart_state.dart';
 
 part 'card.dart';
+part 'gallery.dart';
 part 'page.dart';
 part 'template_editor.dart';
 part 'templates.dart';
