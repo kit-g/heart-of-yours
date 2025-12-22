c2ce5f6d2c672ffe39f3a9e7bafdeb3558d2347d
_kitg <gerasimovkit@gmail.com>
2025-12-21 21:15:14 -0500

    chore: minor

--- lib/presentation/routes/login/google_sign_in/web.dart
+++ lib/presentation/routes/login/google_sign_in/web.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:heart_state/heart_state.dart';
 import 'package:google_sign_in_web/web_only.dart';
+import 'package:heart_state/heart_state.dart';
 
 class GoogleSignInButton extends StatelessWidget {
   final void Function()? onPressed;
@@ -15,25 +15,21 @@ class GoogleSignInButton extends StatelessWidget {
     return FutureBuilder<void>(
       future: Auth.of(context).initGoogleSignIn(),
       builder: (_, future) {
-        switch (future.connectionState) {
-          case ConnectionState.none:
-            return const SizedBox.shrink();
-          case ConnectionState.active:
-          case ConnectionState.waiting:
-            return const SizedBox(
-              width: 28,
-              height: 28,
-            );
-          case ConnectionState.done:
-            return renderButton(
-              configuration: GSIButtonConfiguration(
-                type: GSIButtonType.icon,
-                shape: GSIButtonShape.pill,
-                theme: GSIButtonTheme.outline,
-                size: GSIButtonSize.large,
-              ),
-            );
-        }
+        return switch (future.connectionState) {
+          .none => const SizedBox.shrink(),
+          .active || .waiting => const SizedBox(
+            width: 28,
+            height: 28,
+          ),
+          .done => renderButton(
+            configuration: GSIButtonConfiguration(
+              type: .icon,
+              shape: .pill,
+              theme: .outline,
+              size: .large,
+            ),
+          ),
+        };
       },
     );
   }
