19d3e7f4c3609b6d526df9905a0d9e6c70d78f52
_kitg <gerasimovkit@gmail.com>
2025-12-23 17:54:24 -0500

    test: ChartPreference and related models

---
+++ shared/heart_models/test/mocks.dart
@@ -0,0 +1,11 @@
+import 'package:heart_models/heart_models.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks(
+  [
+    WorkoutSummary,
+    Exercise,
+    ChartPreference,
+  ],
+)
+void main() {}
