b26f2f86b16447ef41e03f418c9552183cbf7ed4
_kitg <gerasimovkit@gmail.com>
2025-12-22 20:31:03 -0500

    feat: chart preference dialog

--- lib/presentation/routes/profile/profile.dart
+++ lib/presentation/routes/profile/profile.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:heart/core/theme/state.dart';
 import 'package:heart/core/utils/misc.dart';
 import 'package:heart/core/utils/scrolls.dart';
+import 'package:heart/core/utils/visual.dart';
 import 'package:heart/presentation/widgets/avatar.dart';
+import 'package:heart/presentation/widgets/buttons.dart';
+import 'package:heart/presentation/widgets/exercises/exercises.dart';
 import 'package:heart_language/heart_language.dart';
 import 'package:heart_models/heart_models.dart';
 import 'package:heart_state/heart_state.dart';
 import 'package:intl/intl.dart';
 
-part 'page.dart';
-
 part 'charts.dart';
+part 'page.dart';
