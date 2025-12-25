343b076bde2e27ab30cc6a66bde6daef89e20785
_kitg <gerasimovkit@gmail.com>
2025-12-25 18:51:26 -0500

    feat: totalRepsHistory metric query in metrics file

--- shared/heart_db/lib/src/metrics.dart
+++ shared/heart_db/lib/src/metrics.dart
@@ -16,3 +16,20 @@ ORDER BY "when" DESC
 LIMIT ?
 ;
 """;
+
+// ✅sum of all reps across all sets performed in a workout
+const getTotalRepsHistory = """
+SELECT
+    sum(coalesce(sets.reps, 0)) AS "value",
+    workouts.start AS "when"
+FROM sets
+INNER JOIN workout_exercises we ON sets.exercise_id = we.id
+INNER JOIN workouts ON we.workout_id = workouts.id
+WHERE workouts.user_id = ?
+  AND we.exercise_id = ?
+  AND sets.completed = 1
+GROUP BY workouts.id
+ORDER BY "when" DESC
+LIMIT ?
+;
+""";
