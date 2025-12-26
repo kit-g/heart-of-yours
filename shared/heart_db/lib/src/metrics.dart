d1fb3aa2f12cb5e6ba5aac3fd4f6317bcbed0d21
_kitg <gerasimovkit@gmail.com>
2025-12-25 19:16:46 -0500

    feat: cardio history metric queries in metrics file

--- shared/heart_db/lib/src/metrics.dart
+++ shared/heart_db/lib/src/metrics.dart
@@ -1,3 +1,34 @@
+const getCardioDurationHistory = """
+SELECT
+    sum(coalesce(sets.duration, 0)) AS "value",
+    workouts.start AS "when"
+FROM sets
+INNER JOIN workout_exercises we ON sets.exercise_id = we.id
+INNER JOIN workouts ON we.workout_id = workouts.id
+WHERE workouts.user_id = ?
+  AND we.exercise_id = ?
+  AND sets.completed = 1
+GROUP BY workouts.id
+ORDER BY "when" DESC
+LIMIT ?
+;
+""";
+
+const getCardioDistanceHistory = """
+SELECT
+    sum(coalesce(sets.distance, 0)) AS "value",
+    workouts.start AS "when"
+FROM sets
+INNER JOIN workout_exercises we ON sets.exercise_id = we.id
+INNER JOIN workouts ON we.workout_id = workouts.id
+WHERE workouts.user_id = ?
+  AND we.exercise_id = ?
+  AND sets.completed = 1
+GROUP BY workouts.id
+ORDER BY "when" DESC
+LIMIT ?
+;
+""";
 
 
 // ✅ for reps-only exercises, we take the best single set
