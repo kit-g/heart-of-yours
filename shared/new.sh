60954056bdffc935450ac9902589a39cb56c30dd
_kitg <gerasimovkit@gmail.com>
2026-01-03 17:12:14 -0500

    feat: analysis options configuration in new.sh

--- shared/new.sh
+++ shared/new.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME="heart_theme" # lowercase and underscores only
 DESCRIPTION="Material app theme"
-ORG="vin.muffin"
+ORG="me.heart-of"
 
 mkdir "$PACKAGE_NAME"
 cd "$PACKAGE_NAME" || exit
@@ -12,6 +12,23 @@ flutter create . \
   --project-name "$PACKAGE_NAME"
 
 mkdir "lib/src"
+
+cat <<EOF > analysis_options.yaml
+include: package:flutter_lints/flutter.yaml
+
+linter:
+  rules:
+    require_trailing_commas: true
+    prefer_const_constructors: true
+    prefer_final_in_for_each: true
+    avoid_positional_boolean_parameters: true
+    avoid_dynamic_calls: true
+    prefer_single_quotes: true
+
+formatter:
+  trailing_commas: preserve
+EOF
+
 rm "lib/$PACKAGE_NAME.dart"
 rm "test/${PACKAGE_NAME}_test.dart"
 echo "library;" >> "lib/$PACKAGE_NAME.dart"
