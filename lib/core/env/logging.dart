7546068396ce795ae70e43fbdf953ee67e5599fe
_kitg <gerasimovkit@gmail.com>
2026-01-22 22:18:50 -0500

    refactor: minor logging refactor

--- lib/core/env/logging.dart
+++ lib/core/env/logging.dart
@@ -1,8 +1,27 @@
 import 'package:logging/logging.dart';
 
+extension on LogRecord {
+  String toLine() {
+    final buffer = StringBuffer()..write('[$loggerName]: ${level.name} - $message');
+    if (error != null) {
+      buffer.write('\n\x1B[31mError: $error\x1B[0m');
+    }
+
+    if (stackTrace != null) {
+      buffer.write('\n$stackTrace');
+    }
+
+    return buffer.toString();
+  }
+
+  // ignore: avoid_print
+  void write() => print(toLine());
+}
+
 void initLogging(String level) {
-  Logger.root.level = _getLevel(level);
-  Logger.root.onRecord.listen(_log);
+  Logger.root
+    ..level = _getLevel(level)
+    ..onRecord.listen((record) => record.write());
 }
 
 Level _getLevel(String v) {
@@ -10,11 +29,6 @@ Level _getLevel(String v) {
     'ALL' => Level.ALL,
     _ => Level.OFF,
   };
-}
-
-void _log(LogRecord record) {
-  // ignore: avoid_print
-  print('[${record.loggerName}]: ${record.level.name} - ${record.message}');
 }
 
 typedef LogInit = void Function(String level);
