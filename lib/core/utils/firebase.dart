34311f0b1bd937a18adfa005749d5bcfcba55f1a
_kitg <gerasimovkit@gmail.com>
2026-06-09 23:13:57 -0400

    refactor: initializeFirebase signature updated to support environment-based options

--- lib/core/utils/firebase.dart
+++ lib/core/utils/firebase.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:heart/firebase_options.dart';
+import 'package:heart/core/env/config.dart';
+import 'package:heart/firebase_options.dart' as dev;
+import 'package:heart/firebase_options_prod.dart' as prod;
 
-Future<FirebaseApp> initializeFirebase() {
+Future<void> initializeFirebase(Env env) {
   return Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: switch (env) {
+      .dev || .test => dev.DefaultFirebaseOptions.currentPlatform,
+      .prod => prod.DefaultFirebaseOptions.currentPlatform,
+    },
   );
 }
