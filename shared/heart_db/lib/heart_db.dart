313b423e59f63f1ef2b2d5f45629f5da05cbdb75
_kitg <gerasimovkit@gmail.com>
2026-09-06 18:17:14 -0400

    feat: add database migration v12 with syncs version and ETag support

--- shared/heart_db/lib/heart_db.dart
+++ shared/heart_db/lib/heart_db.dart
@@ -28,6 +28,7 @@ part 'src/migrations/0008.dart';
 part 'src/migrations/0009.dart';
 part 'src/migrations/0010.dart';
 part 'src/migrations/0011.dart';
+part 'src/migrations/0012.dart';
 part 'src/migrations/index.dart';
 part 'src/parts/charts.dart';
 part 'src/parts/exercises.dart';
